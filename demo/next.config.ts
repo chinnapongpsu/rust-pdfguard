3acea35fd8be3cb4b96373738bb6a09ae4754cbf
Chinnapong Angsuchotmetee <chinnapong.a@psu.ac.th>
2025-06-12 14:05:07 +0700

    Rust PDF Guard v0.1.1

--- demo/next.config.ts
+++ demo/next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import path from "path";
+
+// Extract only the repo name
+const repoName = "rust-pdfguard";
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
+  output: "export",
+
+  webpack: (config, { isServer }) => {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
@@ -15,10 +26,21 @@ const nextConfig: NextConfig = {
       type: "webassembly/async",
     });
 
+    if (!isServer) {
+      config.plugins?.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, "src/rust/rf_validator_bg.wasm"),
+              to: path.resolve(__dirname, "public/rf_validator_bg.wasm"),
+            },
+          ],
+        })
+      );
+    }
+
     return config;
   },
-  output: 'export'
-
 };
 
 export default nextConfig;
